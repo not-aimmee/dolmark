d40ef83895a96d1ad62d7966f44154f91064313b
not-aimmee <aimanfaridi21@gmail.com>
2026-01-13 15:03:36 +0530

    minor changes

--- dolmark-landing/src/app/pages/services/distribution.tsx
+++ dolmark-landing/src/app/pages/services/distribution.tsx
@@ -208,7 +208,7 @@ export function Distribution() {
                   </p>
                    <button
                 onClick={() => setIsModalOpen(true)}
-                className=" text-white px-8 py-3 rounded-md hover:bg-[#14B8A6] transition-colors inline-flex items-center gap-2">
+                className="bg-[#0F2C59] text-white px-8 py-3 rounded-md hover:bg-[#14B8A6] transition-colors inline-flex items-center gap-2">
                 {TEXT.industriesWeServe.button}<span>→</span>
               </button>
               <ContactModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />

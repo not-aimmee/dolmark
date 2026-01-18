a0a6d2af752c20666500a4a7ae076616896bc6ea
not-aimmee <aimanfaridi21@gmail.com>
2026-01-18 16:41:37 +0530

    update

--- dolmark-landing/src/main.tsx
+++ dolmark-landing/src/main.tsx
@@ -3,12 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { HashRouter} from 'react-router-dom'
-const redirect = sessionStorage.redirect;
-delete sessionStorage.redirect;
-
-if (redirect && redirect !== location.pathname) {
-  history.replaceState(null, "", redirect);
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
